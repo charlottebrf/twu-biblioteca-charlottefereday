8339737c960c11ac6176d1e1b82451460546c798
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-13 17:33:41 +0100

    Refactors test to use setBooks() - makes class public to use in tests

--- src/com/twu/biblioteca/Library.java
+++ src/com/twu/biblioteca/Library.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Library {
 
-    private HashMap<String, ArrayList> setBooks() {
+    public HashMap<String, ArrayList> setBooks() {
         HashMap<String, ArrayList> booksInLibrary = new  HashMap<String, ArrayList>();
         ArrayList<String> authorAndPublicationYear = new ArrayList<String>();
         authorAndPublicationYear.add("J.K.Rowling");
--- test/com/twu/biblioteca/LibraryTest.java
+++ test/com/twu/biblioteca/LibraryTest.java
@@ -8,15 +8,9 @@ import static org.junit.Assert.*;
 public class LibraryTest {
 
     @Test
-    public void displaysALibraryBook() {
+    public void displaysAHashMapOfALibraryBook() {
         Library lib = new Library();
-
-        HashMap<String, ArrayList> multiMap = new  HashMap<String, ArrayList>();
-        ArrayList<String> authorAndPublicationYear = new ArrayList<String>();
-        authorAndPublicationYear.add("J.K.Rowling");
-        authorAndPublicationYear.add("1997");
-        multiMap.put("Harry Potter and the Philosopher's Stone", authorAndPublicationYear);
-
+        HashMap<String, ArrayList> multiMap = lib.setBooks();
         assertEquals(multiMap, lib.getBooks());
     }
 }
