fa445e50ee3d18d18da2934c909ac1f10e2cb101
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-14 10:59:27 +0100

    Refactors BookTitle Test

--- test/com/twu/biblioteca/BookTitleTest.java
+++ test/com/twu/biblioteca/BookTitleTest.java
@@ -6,9 +6,10 @@ import static org.junit.Assert.*;
 
 public class BookTitleTest {
 
+    BookTitle title = new BookTitle("Harry Potter and the Philosopher's Stone");
+
     @Test
-    public void createsABookTitleObject() {
-        BookTitle title = new BookTitle("Harry Potter and the Philosopher's Stone");
+    public void getsABookTitle() {
         assertEquals("Harry Potter and the Philosopher's Stone", title.getTitle());
     }
 }
