4c3e975c845b661e4aec35c8b5c50ee41d2c3a06
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-14 11:04:29 +0100

    Adds Year object to be used in Book constructor

--- src/com/twu/biblioteca/Year.java
+++ src/com/twu/biblioteca/Year.java
@@ -1,4 +1,13 @@
 package com.twu.biblioteca;
 
 public class Year {
+    private final int year;
+
+    public Year(int year) {
+        this.year = year;
+    }
+
+    public int getYear() {
+        return this.year;
+    }
 }
--- test/com/twu/biblioteca/YearTest.java
+++ test/com/twu/biblioteca/YearTest.java
@@ -1,5 +1,14 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class YearTest {
+    Year year = new Year(1997);
 
+    @Test
+    public void getsYear() {
+        assertEquals(1997, year.getYear());
+    }
 }
