aa4dcae24f8ec4610f15ddd88d491a4f14626aec
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-14 22:18:39 +0100

    Adds in an Interactive menu for users to choose to see the List of books

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,4 +1,21 @@
 package com.twu.biblioteca;
+import java.util.Scanner;
 
-public class menu {
+public class Menu {
+    private final Library library;
+
+    public Menu(Library library) {
+        this.library = library;
+    }
+
+    public String process(String selection) {
+        String menuChoice = "";
+        switch (selection) {
+            case "1":  menuChoice = this.library.getBookDetails();
+                break;
+        }
+        return menuChoice;
+    }
 }
+
+
--- test/com/twu/biblioteca/MenuTest.java
+++ test/com/twu/biblioteca/MenuTest.java
@@ -1,5 +1,30 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
-public class menuTest {
+public class MenuTest {
+    Library library = new Library();
+    Menu menu = new Menu(library);
 
+    BookTitle firstHP = new BookTitle("Harry Potter and the Philosopher's Stone");
+    Author rowling = new Author("J.K.Rowlng");
+    Year year = new Year(1997);
+    Book harryPotter1 = new Book(firstHP, rowling, year);
+
+    BookTitle secondHP = new BookTitle("Harry Potter and the Chamber of Secrets");
+    Author rowling2 = new Author("J.K.Rowlng");
+    Year year2 = new Year(1998);
+    Book hP2 = new Book(secondHP, rowling2, year2);
+
+    @Test
+    public void hasAnInteractiveMenu() {
+        library.addBooks(hP2);
+        library.addBooks(harryPotter1);
+
+        assertEquals("|" + secondHP.getTitle() + "|" + rowling2.getAuthor() + "|" + year2.getYear() + "|" + "\n" +
+                    "|" + firstHP.getTitle() + "|" + rowling.getAuthor() + "|" + year.getYear() + "|" + "\n",
+                    menu.process("1"));
+    }
 }
