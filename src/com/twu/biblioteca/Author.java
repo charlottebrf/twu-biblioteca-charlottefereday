52cb14d21cc7bc6146515321fa4f46d77bf86f62
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-14 09:36:37 +0100

    Creates an Author object

---
+++ src/com/twu/biblioteca/Author.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class Author {
+    private final String name;
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public Author getAuthor(Author authorObject) {
+        return authorObject;
+    }
+}
---
+++ test/com/twu/biblioteca/AuthorTest.java
@@ -0,0 +1,14 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class AuthorTest {
+
+    @Test
+    public void createsAnAuthorObject() {
+        Author rowling = new Author("J.K.Rowlng");
+        assertEquals(rowling, rowling.getAuthor(rowling));
+    }
+}
