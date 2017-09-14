998a22a263c0b39c61a79bb72ac5dedcfcdc7c79
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-14 16:29:21 +0100

    Refactors printer to make for a more convincing menu

--- src/com/twu/biblioteca/Printer.java
+++ src/com/twu/biblioteca/Printer.java
@@ -4,8 +4,9 @@ public class Printer {
 
     private String welcome = "Welcome to the new Biblioteca App: we are open for business!";
     private String list = "List Books";
+    private String menu = "Main Menu";
 
     public String getWelcome() { return this.welcome; }
 
-    public String mainMenu() { return this.list; }
+    public String mainMenu() { return "****" + this.menu + "****" + "\n\n" + this.list; }
 }
--- test/com/twu/biblioteca/PrinterTest.java
+++ test/com/twu/biblioteca/PrinterTest.java
@@ -7,6 +7,7 @@ public class PrinterTest {
     String welcome = "Welcome to the new Biblioteca App: we are open for business!";
     Printer printer = new Printer();
     String list = "List Books";
+    String menu = "Main Menu";
 
     @Test
     public void welcomeMessage() {
@@ -15,6 +16,6 @@ public class PrinterTest {
 
     @Test
     public void mainMenu() {
-        assertEquals(list, printer.mainMenu());
+        assertEquals("****" + menu + "****" + "\n\n" + list, printer.mainMenu());
     }
 }
