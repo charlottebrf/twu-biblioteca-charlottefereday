babd8e74ffc234c93454dcbdf50939e1c74f9f5f
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-15 18:10:05 +0100

    Printer now prints to console not to strings - refactor

--- src/com/twu/biblioteca/Printer.java
+++ src/com/twu/biblioteca/Printer.java
@@ -1,14 +1,17 @@
 package com.twu.biblioteca;
 
 public class Printer {
-//    Todo: change this to be console logs
+//    Todo: format these so they are centred
     private String welcome = "Welcome to the new Biblioteca App: we are open for business!";
     private String list = "1. List Books\n2. checkout books\n3. Exit";
     private String menu = "Main Menu";
+    private String checkOut = "You have chosen to check out a book, redirecting you now";
 
     public void getWelcome() { System.out.println(welcome); }
 
     public void getMainMenu() { System.out.println(list); }
 
     public void getMenuHeader() { System.out.println("****" + this.menu + "****" + "\n\n"); }
+
+    public void checkOutMessage() { System.out.println(checkOut);}
 }
