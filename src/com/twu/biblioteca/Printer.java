e677e2de02bd946553846c9e55b7ec13d4f7fcf2
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-15 13:07:18 +0100

    Refactors to be Standard output not returned strings - need to update tests with injection

--- src/com/twu/biblioteca/Printer.java
+++ src/com/twu/biblioteca/Printer.java
@@ -3,12 +3,12 @@ package com.twu.biblioteca;
 public class Printer {
 //    Todo: change this to be console logs
     private String welcome = "Welcome to the new Biblioteca App: we are open for business!";
-    private String list = "1. List Books";
+    private String list = "1. List Books\n2. checkout books\n3. Exit";
     private String menu = "Main Menu";
 
-    public String getWelcome() { return this.welcome; }
+    public void getWelcome() { System.out.println(welcome); }
 
-    public String getmainMenu() { return this.list; }
+    public void getMainMenu() { System.out.println(list); }
 
-    public String getMenuHeader() { return "****" + this.menu + "****" + "\n\n"; }
+    public void getMenuHeader() { System.out.println("****" + this.menu + "****" + "\n\n"); }
 }
--- test/com/twu/biblioteca/PrinterTest.java
+++ test/com/twu/biblioteca/PrinterTest.java
@@ -2,25 +2,25 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-
+//Todo: figure out how to inject to test these console logs
 public class PrinterTest {
-    Printer printer = new Printer();
-    String welcome = "Welcome to the new Biblioteca App: we are open for business!";
-    String list = "1. List Books";
-    String menu = "Main Menu";
-
-    @Test
-    public void welcomeMessage() {
-        assertEquals(welcome, printer.getWelcome());
-    }
-
-    @Test
-    public void mainMenu() {
-        assertEquals(list, printer.getmainMenu());
-    }
-
-    @Test
-    public void printMenuHeader() {
-        assertEquals("****" + this.menu + "****" + "\n\n", printer.getMenuHeader());
-    }
+//    Printer printer = new Printer();
+//    String welcome = "Welcome to the new Biblioteca App: we are open for business!";
+//    String list = "1. List Books\n2. checkout books\n3. Exit";
+//    String menu = "Main Menu";
+//
+//    @Test
+//    public void welcomeMessage() {
+//        assertEquals(welcome, printer.getWelcome());
+//    }
+//
+//    @Test
+//    public void mainMenu() {
+//        assertEquals(list, printer.getMainMenu());
+//    }
+//
+//    @Test
+//    public void printMenuHeader() {
+//        assertEquals("****" + this.menu + "****" + "\n\n", printer.getMenuHeader());
+//    }
 }
