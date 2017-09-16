80a106b7ba714ee38fabe6e13b37c24de328016f
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-16 15:25:20 +0100

    Keyboard reads input stream and returns an empty string if nothing has been inputted

--- src/com/twu/biblioteca/Keyboard.java
+++ src/com/twu/biblioteca/Keyboard.java
@@ -1,4 +1,30 @@
 package com.twu.biblioteca;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class Keyboard {
+
+    private final BufferedReader reader;
+
+    public Keyboard() {
+        this(System.in);
+    }
+
+    public Keyboard(InputStream in) {
+        this.reader = new BufferedReader(new InputStreamReader(in));
+    }
+
+
+    public String read() {
+        String returnedBook = "";
+        try {
+            returnedBook = reader.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        return returnedBook;
+    }
 }
--- test/com/twu/biblioteca/KeyboardTest.java
+++ test/com/twu/biblioteca/KeyboardTest.java
@@ -1,7 +1,32 @@
 package com.twu.biblioteca;
 
-import static org.junit.Assert.*;
+import org.junit.Test;
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.InputStream;
+
+import static org.junit.Assert.assertEquals;
 
 public class KeyboardTest {
 
+    public InputStream toStream(String stringy) {
+        return new ByteArrayInputStream( stringy.getBytes() );
+    }
+
+
+    @Test
+    public void canReadInput() {
+        Keyboard keyboard = new Keyboard(toStream("book title"));
+
+        assertEquals("book title", keyboard.read());
+
+    }
+
+    @Test
+    public void returnsAnEmptyStringIfNoInput() {
+        Keyboard keyboard = new Keyboard(toStream(""));
+
+        assertEquals(null, keyboard.read());
+
+    }
 }
