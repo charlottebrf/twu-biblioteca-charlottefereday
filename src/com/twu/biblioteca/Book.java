84a2716afd0e23440bc7627b117c0765cddfdcad
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-16 17:06:55 +0100

    Adds in static final NO_BOOK object

--- src/com/twu/biblioteca/Book.java
+++ src/com/twu/biblioteca/Book.java
@@ -4,6 +4,7 @@ public class Book {
     private final Author author;
     private final Year year;
     public BookTitle title;
+    public static final Book NO_BOOK = new Book(new BookTitle("No book"), new Author("No book"), new Year(1111));
 
     public Book(BookTitle title, Author author, Year year) {
         this.author = author;
@@ -26,4 +27,5 @@ public class Book {
     public int getBookYear() {
         return this.year.getYear();
     }
+
 }
