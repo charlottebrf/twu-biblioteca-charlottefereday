5c167ac7b6d3affbc85507bb6105050f2e0583cb
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-17 11:16:50 +0100

    Refactors returned strings to be System out methods returned by printer- need to refactor tests & Biblioteca to account for void methods

--- src/com/twu/biblioteca/Menu.java
+++ src/com/twu/biblioteca/Menu.java
@@ -1,6 +1,4 @@
 package com.twu.biblioteca;
-import java.io.ByteArrayInputStream;
-import java.util.Scanner;
 
 public class Menu {
     private final Library library;
@@ -20,78 +18,73 @@ public class Menu {
         this(library, book, printer, new Keyboard());
     }
 
-    //Todo: change any strings printed here to be printed by the printer as console logs
-
-    public String checkIsValidOption() {
-        System.out.println("Select a valid option!");
-        return this.exitProgram();
+    public void checkIsValidOption() {
+        printer.selectValidOptionMessage();
+        exitProgram();
     }
 
-    public String exitProgram() {
-        System.out.println("You have selected quit: exiting the program now");
-        return "";
+    public void exitProgram() {
+        printer.exitMenuMessage();
+        System.exit(0);
     }
 
-    public String checkedOutSuccessOrFailureMessage(String stringifiedTitle) {
-        String message = "";
-        if (library.hasBookTitleInLibrary(stringifiedTitle)) {
-            message = "That book is not available.";
+    public void checkedOutSuccessOrFailureMessage(String title) {
+        if (library.hasBookTitleInLibrary(title)) {
+            printer.notAvailableMessage();
         } else {
-            message = "Thank you! Enjoy the book";
+            printer.availableMessage();
         }
-        return message;
     }
 
-    public String returnedSuccessOrFailureMessage(String stringifiedTitle) {
-        String message = "";
-        if (library.hasBookTitleInLibrary(stringifiedTitle)) {
-            message = "Thank you for returning the book.";
+    public void returnedSuccessOrFailureMessage(String title) {
+        if (library.hasBookTitleInLibrary(title)) {
+            printer.returnedMessage();
         } else {
-            message = "That is not a valid book to return.";
+            printer.notReturnedMessage();
         }
-        return message;
     }
 
     //Todo: figure out how I can get the scanner object in here so line 33 can work & refactor as very long
     //Todo: think about responsbility - is the menu the correct place or should this be refactored to the library to check books & printer class to console log
 //    Todo: fix this to add in a book registry: which keeps track of all the books & also asks the user for the title, year & date
-    public String checkOutBook() {
+    public void checkOutBook() {
         String title = keyboard.read();
         if (library.hasBookTitleInLibrary(title)) {
             printer.checkOutMessage();
             Book checkedOutBook = library.findBookFromTitle();
             library.removeBooks(checkedOutBook);
+            checkedOutSuccessOrFailureMessage(title);
+        } else {
+            checkedOutSuccessOrFailureMessage(title);
         }
-        return checkedOutSuccessOrFailureMessage(title);
     }
 
-    public String returnBook() {
+    public void returnBook() {
         String title = keyboard.read();
         //Todo: can't return the book as no book will be returned
         if (!library.hasBookTitleInLibrary(title)) {
             printer.returnMessage();
-
+            returnedSuccessOrFailureMessage(title);
 //            Todo: fix this to add in a book registry: which keeps track of all the books & also asks the user for the title, year & date
 //            library.addBooks(title);
+        } else {
+            returnedSuccessOrFailureMessage(title);
         }
-        return returnedSuccessOrFailureMessage(title);
     }
-
-    //    //Todo: figure out how I can get the scanner object in here so line 33 can work
-    public String process(String selection) {
-        String menuChoice = "";
+    
+    public void process(String selection) {
         switch (selection) {
-            case "1":  menuChoice = library.getBookDetails();
+            case "1":  library.getBookDetails();
                 break;
-            case "2": menuChoice = checkOutBook();
+            case "2": checkOutBook();
                 break;
-            case "3": menuChoice = returnBook();
+            case "3": returnBook();
                 break;
-            default: menuChoice = checkIsValidOption();
+            default: checkIsValidOption();
                 break;
-            case "4": return exitProgram();
+            case "4": exitProgram();
+                break;
         }
-        return menuChoice;
     }
 
 }
--- src/com/twu/biblioteca/Printer.java
+++ src/com/twu/biblioteca/Printer.java
@@ -7,6 +7,12 @@ public class Printer {
     private String menu = "Main Menu";
     private String checkOut = "You have chosen to check out a book, redirecting you now";
     private String returnBook = "You have chosen to return a book, redirecting you now";
+    private String exitMenu = "You have selected quit: exiting the program now";
+    private String selectValidOption = "Select a valid option!";
+    private String notAvailable = "That book is not available.";
+    private String available = "Thank you! Enjoy the book";
+    private String returned = "Thank you for returning the book.";
+    private String notReturned = "That is not a valid book to return.";
 
     public void getWelcome() { System.out.println(welcome); }
 
@@ -14,7 +20,19 @@ public class Printer {
 
     public void getMenuHeader() { System.out.println("****" + this.menu + "****" + "\n\n"); }
 
-    public void checkOutMessage() { System.out.println(checkOut);}
+    public void checkOutMessage() { System.out.println(checkOut); }
 
-    public void returnMessage() { System.out.println(returnBook);}
+    public void returnMessage() { System.out.println(returnBook); }
+
+    public void exitMenuMessage() { System.out.println(exitMenu); }
+
+    public void selectValidOptionMessage() { System.out.println(selectValidOption); }
+
+    public void notAvailableMessage() { System.out.println(notAvailable);}
+
+    public void availableMessage() { System.out.println(available);}
+
+    public void returnedMessage() { System.out.println(returned);}
+
+    public void notReturnedMessage() { System.out.println(notReturned);}
 }
