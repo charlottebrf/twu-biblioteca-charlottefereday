afb6e758835e50a2807144eb433b09bfab8be66c
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-18 17:32:07 +0100

    Creates UserLogin which still needs some work to check validity of a login

--- src/com/twu/biblioteca/UserLogin.java
+++ src/com/twu/biblioteca/UserLogin.java
@@ -1,4 +1,28 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserLogin {
+    final private Map<LibraryNumber, Password> userLogin;
+
+    public UserLogin() {
+        userLogin = new HashMap<LibraryNumber, Password>();
+    }
+
+    public void addUserLogin(LibraryNumber libraryNumber, Password password) {
+        userLogin.put(libraryNumber, password);
+    }
+
+    public UserLogin getUserLogin() {
+        return this;
+    }
+
+
+//    public int findLibraryNumberFromNumber(LibraryNumber libraryNumber) {
+//        if
+//        return userLogin.getOrDefault(libraryNumber.getNumber(),);
+//    }
+
+
 }
--- test/com/twu/biblioteca/UserLoginTest.java
+++ test/com/twu/biblioteca/UserLoginTest.java
@@ -1,5 +1,33 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+import java.util.LinkedList;
+import java.util.Map;
+
 import static org.junit.Assert.*;
 
 public class UserLoginTest {
 
+    Password password = new Password("abcdefgh");
+    BigInteger cell = new BigInteger("0123456789");
+    LibraryNumber libnum = new LibraryNumber(123-4567);
+    UserLogin login = new UserLogin();
+
+    @Before
+    public void setUp() {
+        login = new UserLogin();
+    }
+
+    @Test
+    public void getsUserLoginDetails() {
+        assertEquals(login, login.getUserLogin());
+    }
+
+    @Test
+    public void checksForUserLibraryNumber() {
+        assertEquals(libnum, login.findPasswordFromLibraryNumber());
+        }
 }
