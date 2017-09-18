5eb38304376ae444e7f0f917923638111d4fff31
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-18 16:22:26 +0100

    Creates UserAccount object which can return itself

--- src/com/twu/biblioteca/UserAccount.java
+++ src/com/twu/biblioteca/UserAccount.java
@@ -1,4 +1,29 @@
 package com.twu.biblioteca;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class UserAccount {
+
+    final private LibraryNumber libraryNumber;
+    final private Password password;
+    final private UserName userName;
+    final private UserEmailAddress email;
+    final private UserPhoneNumber phoneNumber;
+    final private  Map<String, Book> booksInAccount;
+
+    public UserAccount(LibraryNumber libraryNumber, Password password, UserName userName, UserEmailAddress email, UserPhoneNumber phoneNumber) {
+        this.libraryNumber = libraryNumber;
+        this.password = password;
+        this.userName = userName;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        booksInAccount = new HashMap<String, Book>();
+    }
+
+    public UserAccount getUserAccount() {
+        return this;
+    }
+
+
 }
--- test/com/twu/biblioteca/UserAccountTest.java
+++ test/com/twu/biblioteca/UserAccountTest.java
@@ -1,5 +1,29 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigInteger;
+
 import static org.junit.Assert.*;
 
 public class UserAccountTest {
+    LibraryNumber libnum = new LibraryNumber(123-4567);
+    Password password = new Password("abcdefgh");
+    UserName userName = new UserName("Charlotte Fereday");
+    UserEmailAddress email = new UserEmailAddress("foo@foo.com");
+    BigInteger cell = new BigInteger("0123456789");
+    UserPhoneNumber number = new UserPhoneNumber(cell);
 
+    UserAccount account = new UserAccount(libnum, password, userName, email, number);
+
+    @Before
+    public void setUp() throws Exception {
+        account = new UserAccount(libnum, password, userName, email, number);
+    }
+
+    @Test
+    public void getsAUserAccount() {
+        assertEquals(account, account.getUserAccount());
+    }
 }
