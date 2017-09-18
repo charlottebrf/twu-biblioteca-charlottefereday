1f7815eb33d44af20c11cf204cb2e783b33cfcaf
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-18 15:21:34 +0100

    Adds tiny type LibraryNumber to be added to the UserAccount constructor: takes an int a constructor

---
+++ src/com/twu/biblioteca/LibraryNumber.java
@@ -0,0 +1,13 @@
+package com.twu.biblioteca;
+
+public class LibraryNumber {
+    private final int number;
+
+    public LibraryNumber(int number) {
+        this.number = number;
+    }
+
+    public int getNumber() {
+        return this.number;
+    }
+}
---
+++ test/com/twu/biblioteca/LibraryNumberTest.java
@@ -0,0 +1,15 @@
+package com.twu.biblioteca;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class LibraryNumberTest {
+    int credential = 123-4567;
+    LibraryNumber number = new LibraryNumber(credential);
+
+    @Test
+    public void getsLibraryNumber() throws Exception {
+        assertEquals(credential, number.getNumber());
+    }
+}
