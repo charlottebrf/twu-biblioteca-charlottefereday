bd39c759b6df11844389795de416c1cbd28156af
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-20 10:52:46 +0100

    Moves test back into main test folder, after experimenting with test subfolders

---
+++ test/com/twu/biblioteca/BookTest.java
@@ -0,0 +1,39 @@
+package com.twu.biblioteca;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class BookTest {
+
+    BookTitle firstHP = new BookTitle("Harry Potter and the Philosopher's Stone");
+    Author rowling = new Author("J.K.Rowlng");
+    Year year = new Year(1997);
+    Book harryPotter1 = new Book(firstHP, rowling, year);
+
+    @Before
+    public void setUp() {
+        harryPotter1 = new Book(firstHP, rowling, year);
+    }
+
+    @Test
+    public void createsABookObject() {
+        assertEquals(harryPotter1, harryPotter1.getBook());
+    }
+
+    @Test
+    public void getsBookTitle() {
+        assertEquals("Harry Potter and the Philosopher's Stone", harryPotter1.title.getTitle());
+    }
+
+    @Test
+    public void getsBookAuthor() {
+        assertEquals("J.K.Rowlng", harryPotter1.getBookAuthor());
+    }
+
+    @Test
+    public void getsYear() {
+        assertEquals(1997, harryPotter1.getBookYear());
+    }
+}
