92cf639a604fef61b53ee3089f62a1c9677d7c9b
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-20 22:09:28 +0100

    Creates FakePrinter test class

--- test/com/twu/biblioteca/FakePrinter.java
+++ test/com/twu/biblioteca/FakePrinter.java
@@ -1,4 +1,15 @@
 package com.twu.biblioteca;
 
-public class FakePrinter {
+public class FakePrinter extends Printer {
+    private String output = "";
+
+    @Override
+    public String display(String stringToBePrinted) {
+        output = stringToBePrinted;
+        return stringToBePrinted;
+    }
+
+    public String output() {
+        return output;
+    }
 }
