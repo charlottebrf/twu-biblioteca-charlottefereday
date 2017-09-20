09cd8f60f98463a50637507daca634bfbb48f47a
Charlotte Fereday <cbrfereday@gmail.com>
2017-09-20 11:40:12 +0100

    Refactors printer test to include a passing test

--- test/com/twu/biblioteca/PrinterTest.java
+++ test/com/twu/biblioteca/PrinterTest.java
@@ -2,13 +2,26 @@ package com.twu.biblioteca;
 
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.io.PrintStream;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
-//Todo: figure out how to inject to test these console logs
-public class PrinterTest {
 
+public class PrinterTest {
+    public InputStream toStream(String stringy) {
+        return new ByteArrayInputStream(stringy.getBytes());
+    }
+    java.io.ByteArrayOutputStream outputStream = new java.io.ByteArrayOutputStream();
+    Printer printer = new Printer();
+
+    @Test
+    public void displaysText() {
+        System.setOut(new java.io.PrintStream(outputStream));
+        System.out.print("Hello world");
+        assertEquals(outputStream.toString(), printer.display("Hello world"));
+    }
 }
